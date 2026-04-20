c2394564cc3044c506c8ca8bde23d2be7bac7ae9
basmalabasmala <pb_rouabah@esi.dz>
2026-04-20 18:01:40 +0100

    Update tp_lip.c

--- tp_lip.c
+++ tp_lip.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tp_lib.h"
+void allocate(word_node *p){
+*p= malloc(sizeof(cell));
 
+}
+/*..................................................................*/
+void FREE(word_node p){
+free(p);
+}
+/*..................................................................*/
+void ass_val(word_node p,int v){
+p->val=v;
+}
+/*..................................................................*/
+void ass_adr(word_node p,pointer q){
+p->addr=q;
+}
+/*..................................................................*/
+int value(word_node p){
+return p->val;
+}
+pointer next(word_node p){
+return p->addr;
+}
