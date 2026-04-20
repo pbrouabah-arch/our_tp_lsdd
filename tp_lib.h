a828e1786d19d2ccbd3f75833e01ec0a15cf022d
basmalabasmala <pb_rouabah@esi.dz>
2026-04-20 18:02:58 +0100

    Update tp_lib.h

--- tp_lib.h
+++ tp_lib.h
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* word node */
+struct word_node {
+    char word[50];
+    struct word_node* next;
+};
+
+/* paragraph node */
+struct paragraph_node {
+    struct word_node* words;
+    struct paragraph_node* next;
+};
+/* file node */
+struct file_node {
+    struct paragraph_node* paragraphs;
+};
+void allocate(word_node *p);
+void FREE(word_node p);
+void ass_val(word_node p,int v);
+void ass_adr(word_node p,word_node q);
+int value(word_node p);
+word_node next(word_node p);
 
