8c2df39df1a3bd6180321e7722841c52de4039f9
pzzenikhri-zineb <pz_zenikhri@esi.dz>
2026-04-23 08:39:02 +0100

    Add print_paragraphs function declaration

--- tp_lib.h
+++ tp_lib.h
@@ -38,4 +38,5 @@ void up_next_para(ptr_paragraph p, ptr_paragraph q); // relie 2 paragraph
 ptr_word return_words(ptr_paragraph p);   // recupere la liste des mots de paragraph
 ptr_paragraph next_para(ptr_paragraph p);  // move to the next p
 void add_paragraph(ptr_paragraph *head, ptr_word w) ; 
+void print_paragraphs(ptr_paragraph head) ;
 
