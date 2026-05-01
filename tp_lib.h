abe8b59b93eafbbc5e7c1ae35282c6590567f3e1
pzzenikhri-zineb <pz_zenikhri@esi.dz>
2026-05-01 13:16:51 +0100

    Refactor tp_lib.h with new structures and functions

--- tp_lib.h
+++ tp_lib.h
@@ -1,11 +1,15 @@
+#ifndef TP_H
+#define TP_H
+
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stdbool.h>
+#include <string.h>
 
 /* word node */
 typedef struct word_node {
     char word[100];
-    struct word_node* addr;   // 
+    struct word_node* addr;
 } word_node;
 typedef word_node* ptr_word;
 
@@ -18,8 +22,11 @@ typedef paragraph_node* ptr_paragraph;
 
 /* file node */
 typedef struct file_node {
-    struct paragraph_node* paragraphs;
+    char filename[100];
+    ptr_paragraph paragraphs;
+    struct file_node* next;
 } file_node;
+typedef file_node* ptr_file;
 
 /* abstract machines of word */
 void allocate(ptr_word *p);
@@ -34,16 +41,24 @@ void free_words(ptr_word h);
 void print_words(ptr_word h);
 
 /* abstract machines of paragraph */
-void alloc_paragraph(ptr_paragraph *p);   // 
+void alloc_paragraph(ptr_paragraph *p);
 void free_paragraph(ptr_paragraph p);
 void up_words(ptr_paragraph p, ptr_word w);
 void up_next_para(ptr_paragraph p, ptr_paragraph q);
 ptr_word return_words(ptr_paragraph p);
 ptr_paragraph next_para(ptr_paragraph p);
 void add_paragraph(ptr_paragraph *head, ptr_word w);
-void print_paragraphs(ptr_paragraph head);
-void print_paragraphs(ptr_paragraph head);
+void print_paragraphs(ptr_paragraph head);   // removed duplicate
 void free_paragraphs(ptr_paragraph head);
-/*----------READ FILES------------------------*/
 ptr_paragraph read_file(char* filename);
 
+/* abstract machines of file */
+void alloc_file(ptr_file *p);
+void free_file(ptr_file p);
+ptr_paragraph get_paragraphs(ptr_file p);
+ptr_file next_file(ptr_file p);
+ptr_file read_files(char* filenames[], int n);
+void print_files(ptr_file head);
+void free_files(ptr_file head);
+
+#endif
