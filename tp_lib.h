e757d6fe54df5dc52124e93e998873583753228b
basmalabasmala <pb_rouabah@esi.dz>
2026-05-07 13:49:17 +0100

    Update tp_lib.h

--- tp_lib.h
+++ tp_lib.h
@@ -44,19 +44,29 @@ void add_word(ptr_word *h, char* v);
 bool exists_word(ptr_word *h, char* v);
 void free_words(ptr_word h);
 void print_words(ptr_word h);
-
+/* abstract machines of phrase */
+void alloc_phrase(ptr_phrase *p);
+void free_phrase(ptr_phrase p);
+void set_phrase(ptr_phrase p, char* s);
+void link_phrase(ptr_phrase p, ptr_phrase q);
+char* get_phrase(ptr_phrase p);
+ptr_phrase next_phrase(ptr_phrase p);
+void add_phrase(ptr_phrase *head, char* s);
+void print_phrases(ptr_phrase head);
+void free_phrases(ptr_phrase head);
+bool exists_phrase(ptr_phrase head, char* s);
 /* abstract machines of paragraph */
+
 void alloc_paragraph(ptr_paragraph *p);
 void free_paragraph(ptr_paragraph p);
-void up_words(ptr_paragraph p, ptr_word w);
-void up_next_para(ptr_paragraph p, ptr_paragraph q);
-ptr_word return_words(ptr_paragraph p);
+void set_phrases(ptr_paragraph p, ptr_phrase ph);
+void link_paragraph(ptr_paragraph p, ptr_paragraph q);
+ptr_phrase get_phrases(ptr_paragraph p);
 ptr_paragraph next_para(ptr_paragraph p);
-void add_paragraph(ptr_paragraph *head, ptr_word w);
-void print_paragraphs(ptr_paragraph head);   // removed duplicate
+void add_paragraph(ptr_paragraph *head, ptr_phrase ph);
+void print_paragraphs(ptr_paragraph head);
 void free_paragraphs(ptr_paragraph head);
 ptr_paragraph read_file(char* filename);
-
 /* abstract machines of file */
 void alloc_file(ptr_file *p);
 void free_file(ptr_file p);
